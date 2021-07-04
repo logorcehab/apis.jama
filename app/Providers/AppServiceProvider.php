3f3e93f1573a068eb420422a2d17f34017329bd7
Kelvin Nketia-Achiampong <achiampongk22@gmail.com>
2021-07-04 17:10:28 +0000

    Social authentication added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Coderello\SocialGrant\Resolvers\SocialUserResolverInterface;
+use App\Services\SocialUserResolver;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        $this->app->bind(SocialUserResolverInterface::class, SocialUserResolver::class);
     }
 
     /**
